6dc812df98242aa1e183938a390d8fc1452f24f6
JubJub6321 <JubJub632@gmail.com>
2025-10-06 12:54:45 -0600

    Update Home.js. Making progress. Still need to test.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./Page.css";
 
 function Home() {
+  const location=useLocation()
+  let 
   return (
     <div className="page">
-      <h1>Welcome to ASL Translator</h1>
+      <h1>Hello {location.state.id}! Welcome to ASL Translator</h1>
       <p>Learn, practice, and quiz yourself on ASL signs.</p>
     </div>
   );
