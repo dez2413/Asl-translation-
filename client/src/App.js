59d9d4bbbd834c2070bd0533ed25cc67f3e9ddf6
JubJub6321 <JubJub632@gmail.com>
2025-10-06 11:37:01 -0600

    Update App.js. Following the tutorial. Don't merge until we can confirm that it works.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {useState} from "react"
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Lessons from "./pages/Lessons";
@@ -10,17 +11,19 @@ import Sign from "./pages/Sign";
 
 function App() {
   return (
-    <Router>
+    <div className="App">
+      <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/lessons" element={<Lessons />} />
-        <Route path="/practice" element={<Practice />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Sign" element={<Sign />} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lessons" element={<Lessons />} />
+          <Route path="/practice" element={<Practice />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Sign" element={<Sign />} />
+        </Routes>
+      </Router>
+  </div>
   );
 }
 
