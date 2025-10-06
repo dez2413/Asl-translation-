a4070c5faa2b549a9f99531aeb2f65c5320ca17d
dez2413 <157758680+dez2413@users.noreply.github.com>
2025-10-06 15:32:03 -0600

    fix server

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -22,51 +22,54 @@ function Login() {
       await axios.post("mongodb+srv://jubjub632_db_user:MAxn3eRWMDwmR4o@asldatabase.9sn35at.mongodb.net/?retryWrites=true&w=majority&appName=ASLDatabase"),{
         email,password
       })
-      .then(res=>{
-        if(res.data=="exist"){
-          history("/home",{state:{id:email}})
-        } else if(res.data=="notexist){
-          alert("User not signed up.")
-        })
-      .catch(e=>{})
+
+    // then(res=>{
+    //     if(res.data=="exist"){
+    //       history("/home",{state:{id:email}})
+    //     } 
+    //     else if(res.data=="notexist){
+    //       alert("User not signed up.")
+    // })
+      catch(e=>{})
         alert{"Wrong password")
               console.log(e);
     } catch(e){
       console.log(e);
     }
   
-  return (
-    <div className="container">
-      <div className="header">
-        <div className ="text"> Login</div>
-        <div className="Underline"></div>
-      </div>
-      <div className = "inputs">
-        <div className = "input">
-            <img src={email_icon} alt=""/>
-            <input type="email" onChange={(e)=>(setEmail(e.target.value))} placeholder="Email" />
+    return (
+        <div className="container">
+        <div className="header">
+            <div className ="text"> Login</div>
+            <div className="Underline"></div>
         </div>
-        <div className = "input">
-            <img src={passwaord_icon} alt=""/>
-            <input type="password" onChange={(e)=>(setPassword(e.target.value))} placeholder="Password" />
+        <div className = "inputs">
+            <div className = "input">
+                <img src={email_icon} alt=""/>
+                <input type="email" onChange={(e)=>(setEmail(e.target.value))} placeholder="Email" />
+            </div>
+            <div className = "input">
+                <img src={passwaord_icon} alt=""/>
+                <input type="password" onChange={(e)=>(setPassword(e.target.value))} placeholder="Password" />
+            </div>
+            <input type="submit" onClick={submit}/>
+
+            </form>
+
+            <br />
+            <p>OR</p>
+            <br />
+
+            <Link to="/sign"></Link>Sign
         </div>
-          <input type="submit" onClick={submit}/>
-
-          </form>
-
-          <br />
-          <p>OR</p>
-          <br />
-
-          <Link to="/sign"></Link>Sign
-      </div>
-      <div className= "forgot-password">Lost Password? <span>Click Here!</span></div>
-      <div className ="submit-container">
-        <Link to="/sign" className="submit">Sign Up</Link>
-        <Link to="/login" className="submit">Login</Link>
-      </div>
-    </div>
-  );
+        <div className= "forgot-password">Lost Password? <span>Click Here!</span></div>
+        <div className ="submit-container">
+            <Link to="/sign" className="submit">Sign Up</Link>
+            <Link to="/login" className="submit">Login</Link>
+        </div>
+        </div>
+    );
 }
+
 
 export default Login;
