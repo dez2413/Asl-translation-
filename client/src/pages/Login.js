ce506bf79869a4274a55d577385dcd4c14b57bfa
JubJub6321 <JubJub632@gmail.com>
2025-10-06 12:15:21 -0600

    Login should be setup but some things are different from the tutorial.
    There is a <h1>Signup<h1> line in the video and a <form action="POST"> line in the video. I don't know if they are necessary or if this is just because he is using a different web format.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,6 +8,21 @@ import email_icon from "../assets/email.png";
 import passwaord_icon from "../assets/password.png";
 
 function Login() {
+
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+
+  async function submit(e){
+    e.preventDefault();
+
+    try{
+      await axios.post("mongodb+srv://jubjub632_db_user:MAxn3eRWMDwmR4o@asldatabase.9sn35at.mongodb.net/?retryWrites=true&w=majority&appName=ASLDatabase"),{
+        email,password
+      })
+    } catch(e){
+      console.log(e);
+    }
+  
   return (
     <div className="container">
       <div className="header">
@@ -17,12 +32,21 @@ function Login() {
       <div className = "inputs">
         <div className = "input">
             <img src={email_icon} alt=""/>
-            <input type="email" placeholder="Email" />
+            <input type="email" onChange={(e)=>(setEmail(e.target.value))} placeholder="Email" />
         </div>
         <div className = "input">
             <img src={passwaord_icon} alt=""/>
-            <input type="password" placeholder="Password" />
+            <input type="password" onChange={(e)=>(setPassword(e.target.value))} placeholder="Password" />
         </div>
+          <input type="submit" onClick={submit}/>
+
+          </form>
+
+          <br />
+          <p>OR</p>
+          <br />
+
+          <Link to="/sign"></Link>Sign
       </div>
       <div className= "forgot-password">Lost Password? <span>Click Here!</span></div>
       <div className ="submit-container">
