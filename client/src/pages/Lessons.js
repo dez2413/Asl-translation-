b41d0f15e858560d5dd918f7a07e4a1a60ef6aaa
dez2413 <157758680+dez2413@users.noreply.github.com>
2025-10-08 14:50:06 -0600

    added change to lesson

--- client/src/pages/Lessons.js
+++ client/src/pages/Lessons.js
@@ -1,15 +1,16 @@
 import React from "react";
-import {useNavigate, useLocation} from "react-router-dom";
-import "./style/Page.css";;
+// import {useNavigate, useLocation} from "react-router-dom";
+import "./style/Page.css";
+import "./style/Lesson.css"
 
 function Lessons() {
-  const location = useLocation();
+  // const location = useLocation();
   
   return (
     <div className="page">
       <h1>Lessons</h1>
-      <h2>Hello {location.state.id} </h2>
-      <p>Here you will find ASL alphabet and word lessons.</p>
+      {/* <h2>Hello {location.state.id} </h2> */}
+      
     </div>
   );
 }
