6568898283534ff52f8bf4e9cc517b6ecb297e38
Tiare-nmsu <tiare-ac@nmsu.edu>
2025-10-18 15:32:07 -0600

    @mediapipe/tasks-vision add issue

--- client/src/pages/Practice.js
+++ client/src/pages/Practice.js
@@ -1,8 +1,25 @@
 import React, { useRef, useEffect } from "react";
-import { Hands } from "@mediapipe/hands";
+import { Hands, HAND_CONNECTIONS } from "@mediapipe/hands";
 import { Camera } from "@mediapipe/camera_utils";
 import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+import { GestureRecognizer, FilesetResolver, DrawingUtils } from "@mediapipe/tasks-vision";
+//"https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22-rc.20250304/+esm";
+//"@mediapipe/tasks-vision";
 import "./style/Page.css";
+
+function GestureTest(){
+
+  return;
+}
+
+function processResult(){
+  console.log("entered process Result");
+  return(
+    <script>
+      console.log("return from procRes");
+    </script>
+  );
+}
 
 function Practice() {
   const videoRef = useRef(null);
@@ -39,7 +56,7 @@ function Practice() {
 
       if (results.multiHandLandmarks) {
         for (const landmarks of results.multiHandLandmarks) {
-          drawConnectors(canvasCtx, landmarks, Hands.HAND_CONNECTIONS, {
+          drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
             color: "#00FF00",
             lineWidth: 2,
           });
@@ -64,6 +81,48 @@ function Practice() {
     }
   }, []);
 
+    // setup model usage
+  
+    //wasm path
+    const vision = FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22/wasm"
+    );
+
+    const gestureRecognizer = new GestureRecognizer.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath: "./handgesture_recognizer.task"
+    },
+    runningMode: "video",
+    numHands: 2
+    });
+
+/*
+    //canvasCtx exist
+    //const gestureOutput = document.getElementById("gesture_output");
+
+  // set up recognition
+  //gestureRecognizer.setOptions({ runningMode: "VIDEO" });
+
+  let lastVideoTime = -1;
+  function renderLoop(){
+    //const video = document.getElementById("video");
+
+    if (video.currentTime !== lastVideoTime) {
+      const gestureRecognitionResult = gestureRecognizer.recognizeForVideo(video);
+      processResult(gestureRecognitionResult);
+      lastVideoTime = video.currentTime;
+    }
+
+    requestAnimationFrame(() => {
+      renderLoop();
+    });
+  }*/
+
+  // https://ai.google.dev/edge/mediapipe/solutions/vision/gesture_recognizer
+  // https://ai.google.dev/edge/api/mediapipe/js/tasks-vision.gesturerecognizer#gesturerecognizercreatefrommodelbuffer
+  // https://codepen.io/mediapipe-preview/pen/zYamdVd
+  // https://ai.google.dev/edge/mediapipe/solutions/vision/gesture_recognizer/web_js#video
+  // score and categoryName of handedness
   return (
     <div className="page p-6 text-center">
       <h1 className="text-3xl font-bold mb-4">Practice</h1>
