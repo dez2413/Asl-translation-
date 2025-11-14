5f63c6e58b9a3128cded2674d9bf9012ebb1d90f
dez2413 <157758680+dez2413@users.noreply.github.com>
2025-11-14 12:45:44 -0700

    Update home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,43 +1,53 @@
-import React from "react";             // Importing React library
+import React, { useContext } from "react";             // Importing React library
 import "./style/Page.css";             // General page styling
 import "./style/Home.css";             // Home page specific styling
 import { Link } from "react-router-dom"; // For internal navigation links
+import { AuthContext } from "../context/AuthContext"; // For auth user 
 
-// import home_image from "../assets/home_image.png";
 import arrow_icon from "../assets/arrow.png";
 
 function Home() {
+  const { user } = useContext(AuthContext); // ⬅️ get logged in user
+
   return (
     <div className="home-container">
-      {/* Left Section */}
+
       <div className="home-left">
-        {/* Hashtag Section */}
         <div className="hashtag">#ASL Translate</div>
 
-        {/* Title and Description */}
         <h1 className="home-title">ASL Translate Goal's</h1>
         <p className="home-info">
-         This project helps people learn ASL through video lessons, 
-         quizzes, and camera-based gesture tracking, guiding them 
-         from basic words to full sentences with the option of 
-         one-on-one practice with an instructor.
+           This project helps people learn ASL through video lessons, 
+           quizzes, and camera-based gesture tracking, guiding them 
+           from basic words to full sentences with the option of 
+           one-on-one practice with an instructor.
         </p>
 
-        {/* Get Started Button */}
-        <Link to="/sign">
-          <button className="home-button">Get Started 
-            <div className="arrow-circle">
-              <img src={arrow_icon} alt="" className="arrow-icon" />
-            </div>
-          </button>
-        </Link>
+        {/* 👇 CONDITIONAL BUTTON BASED ON LOGIN */}
+        {user ? (
+          <Link to="/lessons">
+            <button className="home-button">
+              Go to Lessons
+              <div className="arrow-circle">
+                <img src={arrow_icon} className="arrow-icon" />
+              </div>
+            </button>
+          </Link>
+        ) : (
+          <Link to="/sign">
+            <button className="home-button">
+              Get Started
+              <div className="arrow-circle">
+                <img src={arrow_icon} className="arrow-icon" />
+              </div>
+            </button>
+          </Link>
+        )}
       </div>
 
-      {/* Right Section */}
-      <div className="home-right">
+      <div className="home-right"></div>
         {/* Placeholder for future image or content */}
         {/* <img src={home_image} alt="Home" className="home-image" /> */}
-      </div>
     </div>
   );
 }
